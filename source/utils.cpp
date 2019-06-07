ec534aefd5d6bf633efa5bde2c13e37cd708a2d7
Lorenzo Iuri <lore9625@gmail.com>
2019-06-07 21:04:29 +0200

    cleanup

--- source/utils.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "utils.h"
-
-int map(int value, int istart, int istop, int ostart, int ostop) {
-    return ostart + (ostop - ostart) * (((value - istart)*1.0) / ((istop - istart)-1.0));
-}
--- source/utils.h
+++
@@ -1,2 +0,0 @@
-
-int map(int value, int istart, int istop, int ostart, int ostop);
